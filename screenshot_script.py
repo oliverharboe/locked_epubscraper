f8118c6c2630d85c742365b31438f698dcf21468
Oliver Harboe <Oli.harboe@gmail.com>
2025-04-17 18:28:01 +0200

    small change

--- screenshot_script.py
+++ screenshot_script.py
@@ -2,33 +2,28 @@ import pyautogui
 from fpdf import FPDF
 import time
 import os
-# Opret et Word dokument
 pdf = FPDF()
 
-# Antal screenshots du vil tage
 
-antal_screenshots = int(input("Hvor mange sider? "))
-_ = input("Tryk enter for at starte ")
+antal_screenshots = int(input("Pages in the ebook: "))
+_ = input("Enter to start the 7sec countdown ")
 for i in range(7):
-    print(f'starter om {7-i} sekunder')
+    print(f'starting in {7-i} seconds')
     time.sleep(1)
 for i in range(antal_screenshots):
-    # Tag et screenshot og gem midlertidigt
     screenshot_path = f"screenshot_{i}.png"
     screenshot = pyautogui.screenshot(region=(500, 100, 1300,1100 ))
     screenshot.save(screenshot_path)
 
-    # Tilføj screenshot til Word dokumentet
     
     pdf.add_page()
     pdf.image(screenshot_path, x=0, y=7, w=pdf.w, h=pdf.h-100)
     
-    # Vent før næste screenshot
     pyautogui.click(2100,645)
-    time.sleep(0.1)  # vent 2 sekunder, kan justeres
+    time.sleep(0.1)  
 
     if os.path.exists(screenshot_path):
         os.remove(screenshot_path)
-# Gem dokumentet
+# save pdf
 pdf.output("screenshot_pdf.pdf")
-print("Alle screenshots gemt og dokumentet er klar!")
+print("Done")
